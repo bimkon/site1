2d20d0a5e4bf128f4d888004ca9404553d79fa4e
bimkon <lexakutsenko@mail.ru>
2020-04-16 11:04:15 +0400

    made text fields and checkbox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import 'jquery-ui/ui/widgets/datepicker.js';
+import 'jquery-ui/ui/widgets/slider.js';
+import 'jquery-ui/themes/base/all.css';
 function importAll(resolve) {
     resolve.keys().forEach(resolve);
   }
 importAll(require.context('../src/', true, /\.js$|\.scss$/));
+// require("node_modules/jquery-ui/themes/black-tie/jquery-ui.css");
+// require("node_modules/jquery-ui/themes/black-tie/jquery-ui.theme.css");
