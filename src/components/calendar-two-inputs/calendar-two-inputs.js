8c41e0533bc52a17cf5650c2197dcde99c8dbf5e
bimkon <lexakutsenko@mail.ru>
2020-09-26 14:23:42 +0400

    calendar two inputs

---
+++ src/components/calendar-two-inputs/calendar-two-inputs.js
@@ -0,0 +1,18 @@
+$('.datepicker-two-inputs').datepicker({
+    inline: false,
+    clearButton: true,
+    dateFormat: 'dd.mm.yyyy',
+    onSelect: function (fd, d, picker) { 
+        $(".datepicker__start").val(fd.split("-")[0]);
+        $(".datepicker__end").val(fd.split("-")[1]);
+        },
+    navTitles: {
+        days: 'MM yyyy'
+    },
+    prevHtml: '<i classs="ma">arrow_back</i>',
+    nextHtml: '<i classs="ma">arrow_forward</i>'
+});
+// кнопка уже добавляется другими календарями
+// var okButton = '<span class="datepicker--button datepicker--button-apply" data-action="hide">Применить</span>'; 
+// $('.datepicker--button[data-action="clear"]').each(function( index ) { $(okButton).insertAfter($(this)); });
+
