b635406873ecb3e1d0630933cf8966a37e4e8a3d
bimkon <lexakutsenko@mail.ru>
2020-11-22 23:18:55 +0400

    made oage cards
    made all forms

--- src/components/calendar-two-inputs/calendar-two-inputs.js
+++ src/components/calendar-two-inputs/calendar-two-inputs.js
@@ -12,6 +12,14 @@ $('.datepicker-two-inputs').datepicker({
     prevHtml: '<i classs="ma">arrow_back</i>',
     nextHtml: '<i classs="ma">arrow_forward</i>'
 });
+
+
+    if($('.datepicker-here').hasClass('inline')){
+        $('.datepicker-here').datepicker({
+            inline: true
+        })
+    }
+ 
 // кнопка уже добавляется другими календарями
 // var okButton = '<span class="datepicker--button datepicker--button-apply" data-action="hide">Применить</span>'; 
 // $('.datepicker--button[data-action="clear"]').each(function( index ) { $(okButton).insertAfter($(this)); });
