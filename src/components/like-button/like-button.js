4c6e1802757939dfa430597673d791d717eb6f34
bimkon <lexakutsenko@mail.ru>
2021-06-22 01:13:04 +0400

    Исправил структуру данных #3

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -3,10 +3,10 @@ $('.like-button_type_counter').on('click', function(event, count) {
     
     var $this = $(this),
         count = $this.attr('data-count'),
-        active = $this.hasClass('active'),
+        active = $this.hasClass('like-button_type_active'),
         multiple = $this.hasClass('multiple-count');
     
     $.fn.noop = $.noop;
-    $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
+    $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('like-button_type_active');
     
   });
